69c411f690ddd2eaea6a19fce05e2d016dc4cc1b
Ryan Campbell <me@ryancampbell.name>
2017-03-31 00:32:05 -0700

    Some upgrades must happen

--- viral.creep.setup.upgrader.js
+++ viral.creep.setup.upgrader.js
@@ -6,6 +6,7 @@ setup._maxCount = function(room){
       //        case 'ROOMNAME':
       //            return Math.max(1, count - 1); // one fewer
       case 'W47N66':
+      case 'W47N62':
                   return count + 1; // one more
       default:
           return count;
