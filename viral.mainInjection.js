966ea6f47622d043adc3743483db42428066a39e
Ryan Campbell <me@ryancampbell.name>
2017-04-02 23:49:15 -0700

    sourceKillers can't spawn in rooms below 1800 energy!!!!!!

--- viral.mainInjection.js
+++
@@ -1,7 +0,0 @@
-    Task.guard.creep.guard = {
-        fixedBody: [RANGED_ATTACK, TOUGH, MOVE, MOVE],
-        multiBody: [RANGED_ATTACK, RANGED_ATTACK, HEAL, MOVE, MOVE, MOVE],
-        name: "guard",
-        behaviour: "ranger",
-        queue: 'Low'
-    };
